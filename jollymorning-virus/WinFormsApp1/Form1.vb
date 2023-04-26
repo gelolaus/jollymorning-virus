bdb35b94f6251b4be12500f6755141f677911245
Angelo Laus <angelolausofcl@gmail.com>
2023-04-26 10:47:26 +0800

    Update virus

--- jollymorning-virus/WinFormsApp1/Form1.vb
+++ jollymorning-virus/WinFormsApp1/Form1.vb
@@ -2,12 +2,5 @@
     Private Sub btn_close_Click(sender As Object, e As EventArgs) Handles btn_close.Click
         Dim form As New Form1() ' Replace Form1 with the name of your form
         form.Show() ' Show the form
-        form.Show() ' Show the form
-        form.Show() ' Show the form
-        form.Show() ' Show the form
-        form.Show() ' Show the form
-        form.Show() ' Show the form
-        form.Show() ' Show the form
-        form.Show() ' Show the form
     End Sub
 End Class
